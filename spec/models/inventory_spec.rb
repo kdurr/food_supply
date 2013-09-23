b3b40c0cd339cc09f9184a7fff50971586cc26e5
kdurr <kristina.l.durr@gmail.com>
2013-09-23 16:08:29 -0400

    added validation tests

--- spec/models/inventory_spec.rb
+++ spec/models/inventory_spec.rb
@@ -6,4 +6,12 @@ describe Inventory do
   it { should validate_presence_of :description }
   it { should validate_presence_of :quantity }
 
+  it { should have_valid(:title).when('Wheat Thins', 'Beef Jerky') }
+  it { should_not have_valid(:title).when(nil, '') } 
+
+  it { should have_valid(:description).when('Original', 'Smoked') }
+  it { should_not have_valid(:description).when(nil, '') } 
+
+  it { should have_valid(:quantity).when(250, 100) }
+  it { should_not have_valid(:quantity).when(nil, '') } 
 end
